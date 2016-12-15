01f4cdbe3bd140e7431876354c63f89a32c278cc
Jason Davidson <jason@jljdavidson.com>
2016-12-15 11:47:55 -0700

    Initial mass refund

---
+++ src/fwp/alsaccount/refund/action/MassRefundApproval.java
@@ -0,0 +1,20 @@
+package fwp.alsaccount.refund.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author jljdavidson on 12/15/16.
+ */
+public class MassRefundApproval extends ActionSupport {
+	private static final long serialVersionUID = 2934760737704835921L;
+
+	@Override
+	public String input() throws Exception {
+		return super.input();
+	}
+
+	@Override
+	public String execute() throws Exception {
+		return super.execute();
+	}
+}
